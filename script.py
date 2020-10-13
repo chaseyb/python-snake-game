c7c186cdf8d644d8f7086c14b4b9d78582c238be
Chase Johnson <Chase.b.johnson@gmail.com>
2020-10-12 21:55:35 -0700

    Setup script file and defined game objects.

---
+++ script.py
@@ -0,0 +1,12 @@
+# Python Modules 
+import pygame
+import sys 
+import random
+
+# defines the snake 
+class snake(object):
+    pass
+
+# defines food the snake eats
+class food(object):
+    pass 
