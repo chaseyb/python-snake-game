dfc71bca875d0a4e0b1d92d7dfa215167327896e
Chase Johnson <Chase.b.johnson@gmail.com>
2020-10-12 22:03:30 -0700

    Added methods for snake and food variables.

--- script.py
+++ script.py
@@ -5,8 +5,35 @@ import random
 
 # defines the snake 
 class snake(object):
-    pass
+    def __init__(self):
+        pass
     
+    def get_head_position(self):
+        pass
+
+    def turn(self, point):
+        pass
+    
+    def move(self):
+        pass
+    
+    def reset(self):
+        pass
+        
+    def draw(self, surface):
+        pass
+    
+    def handle_keys(self):
+        pass
+
 # defines food the snake eats
 class food(object):
-    pass 
+    def __init__(self):
+        pass
+
+    def randomize_position(self):
+        pass
+
+    def draw(self, surface):
+        pass
+         
