cf14158078b0c19854f80e1cf8f08a31f4932a67
Chase Johnson <Chase.b.johnson@gmail.com>
2020-10-13 21:52:50 -0700

    Finished defining the Snake.

--- script.py
+++ script.py
@@ -10,27 +10,52 @@ class snake(object):
         self.positions = [((SCREEN_WIDTH / 2), (SCREEN_HEIGHT /2))]
         self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
         self.color = (17, 24, 47)
-    
+
     def get_head_position(self):
         return self.positions[0]
-
+ 
     def turn(self, point):
         if self.length > 1 and (point[0] * -1, point[1] * -1) == self.direction:
             return
         else:
             self.direction = point
-            
+   
     def move(self):
-        pass
-    
+        cur = self.get_head_position()
+        x, y = self.direction
+        new = ((cur[0] + (x*GRIDSIZE)) % SCREEN_WIDTH), (cur[1] + (y*GRIDSIZE)) % SCREEN_HEIGHT)
+        if len(self.positions) > 2 and new in self.positions[2:]:
+            self.reset()
+        else:
+            self.positions.insert(0, new)
+            if len(self.positons) > self.length:
+                self.positions.pop()
+  
     def reset(self):
-        pass
-        
+        self.length = 1
+        self.positons = [((SCREEN_WIDTH / 2), (SCREEN_HEIGHT /2))]
+        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
+
     def draw(self, surface):
-        pass
-    
+        for p in self.positions:
+            r = pygame.Rect((p[0], p[1], (GRIDSIZE, GRIDSIZE))
+            pygame.draw.rect(surface, self.color, r)
+            pygame.draw.rect(surface, (93, 216, 228), r, 1)
+ 
     def handle_keys(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.turn(UP)
+                elif event.type == pygame.K_DOWN:
+                    self.turn(DOWN)
+                elif event.type == pygame.K_LEFT:
+                    self.turn(LEFT)
+                elif event.type == pygame.K_RIGHT:
+                    self.turn(RIGHT)
 
 # defines food the snake eats
 class food(object):
